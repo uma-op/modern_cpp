eac427e5f4ae940cd1ff36d097d8a0bb1af0c955
Arseniy Bannikov <bannikovtop@gmail.com>
2023-04-07 16:30:20 +0300

    Добавлена справка

--- tail/src/main.cpp
+++ tail/src/main.cpp
@@ -10,8 +10,17 @@
 
 int main(int argc, char** argv) {
     if (argc != 2) {
-        std::cout << "Usage: ";  // TODO: дописать справку
+        std::cout
+            << "Usage: \n"
+            << "    tail '[INSTRUCTION [| INSTRUCTION ...]]' \n\n"
+            << "    INCTRUCTION\n"
+            << "        echo    <some strign>   - transits 'some string' to next operation\n"
+            << "        cat     <filename.txt>  - transits own input and content of 'filename.txt' to next operation\n"
+            << "        tail    <N>             - transits last 'N' strings of input to next operation\n";
+
+        return 0;
     }
+
 
     pipeline pl(argv[1]);
     pl.start_processing();
